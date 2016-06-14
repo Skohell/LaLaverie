081b3a12e718904125f983bb8e43c3039fc1c3c6
Guillaume <guillaume.gravallon@gmail.com>
2016-06-14 18:22:37 +0200

    Add files via upload

---
+++ LaLaverieProject/Model/BiereModel.cs
@@ -0,0 +1,443 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using BusinessLayer.Entities;
+
+namespace LaLaverie.Model
+{
+    public class BiereModel : NotifyPropertyChangedBase
+    {
+
+        #region Listes static 
+        /// <summary>
+        /// Liste de toutes les catégories de bières
+        /// </summary>
+        public static List<string> categories = new List<string>();
+        /// <summary>
+        /// Liste de tous les types de bière
+        /// </summary>
+        public static List<string> types = new List<string>();
+        /// <summary>
+        /// Liste de toutes les bières 
+        /// </summary>
+        public static List<Biere> bieres = new List<Biere>();
+        /// <summary>
+        /// Liste d'ustensiles
+        /// </summary>
+        public static Dictionary<Stock, int> ustensiles = new Dictionary<Stock, int>();
+        /// <summary>
+        /// Liste d'ingrédients --------------------- A revoir ---------------------------
+        /// </summary>
+        public static Dictionary<Stock, int> ingredients = new Dictionary<Stock, int>();
+        #endregion
+
+        #region Propriétés de la classe
+
+        /// <summary>
+        /// Lien vers l'image de la bière
+        /// </summary>
+        private string _imageUrl;
+        public string ImageUrl
+        {
+            get
+            {
+                return _imageUrl;
+            }
+
+            set
+            {
+                _imageUrl = value;
+            }
+        }
+
+        /// <summary>
+        /// Nom de la bière
+        /// </summary>
+        private string _nom;
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("Le nom de la bière ne peut être nul.");
+                else
+                {
+                    _nom = value;
+                    NotifyPropertyChanged("Nom");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Catégorie de la bière
+        /// </summary>
+        private string _categorie;
+        public string Categorie
+        {
+            get
+            {
+                return _categorie;
+            }
+            set
+            {
+                //if (!categories.Contains(value))
+                //    throw new Exception("Cette catégorie de bière n'existe pas.");
+                //else
+                //{
+                    _categorie = value;
+                    NotifyPropertyChanged("Categorie");
+                //}
+            }
+        }
+
+        /// <summary>
+        /// Type de la bière
+        /// </summary>
+        private string _type;
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                //if (!types.Contains(value))
+                //    throw new Exception("Ce type de bière n'existe pas.");
+                //else
+                //{
+                    _type = value;
+                    NotifyPropertyChanged("Type");
+                //}
+            }
+        }
+
+        /// <summary>
+        /// Descritpion de la bière
+        /// </summary>
+        private string _description;
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    _description = "Aucune description pour cette bière.";
+                else
+                {
+                    _description = value;
+                    NotifyPropertyChanged("Description");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Taux d'alcool de la bière
+        /// </summary>
+        private float _alcool;
+        public float Alcool
+        {
+            get
+            {
+                return _alcool;
+            }
+            set
+            {
+                if (!(value >= 0 && value <= 100))
+                    throw new Exception("Le taux d'alcool doit être compris entre 0 et 100.");
+                else
+                {
+                    _alcool = value;
+                    NotifyPropertyChanged("Alcool");
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Taux d'amertume de la bière
+        /// </summary>
+        private int _amertume;
+        public int Amertume
+        {
+            get
+            {
+                return _amertume;
+            }
+            set
+            {
+                if (!(value >= 0 && value <= 10))
+                    throw new Exception("L'amertume doit être comprise entre 0 et 10.");
+                else
+                {
+                    _amertume = value;
+                    NotifyPropertyChanged("Amertume");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prix de la bière
+        /// </summary>
+        private float _prix;
+        public float Prix
+        {
+            get
+            {
+                return _prix;
+            }
+            set
+            {
+                if (value <= 0 || value.Equals(null))
+                    throw new Exception("Le prix ne peut être nul.");
+                else
+                {
+                    _prix = value;
+                    NotifyPropertyChanged("Prix");
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Nombre de bouteilles disponibles
+        /// </summary>
+        private int _nbBouteille;
+        public int NbBouteille
+        {
+            get
+            {
+                return _nbBouteille;
+            }
+            set
+            {
+                if (!value.Equals(null))
+                    _nbBouteille = value;
+                NotifyPropertyChanged("NbBouteille");
+            }
+        }
+
+
+        /// <summary>
+        /// Empatage
+        /// </summary>
+        private string _empatage;
+        public string Empatage
+        {
+            get
+            {
+                return _empatage;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("Le maltage doit avoir des consignes.");
+                else
+                {
+                    _empatage = value;
+                    NotifyPropertyChanged("Empatage");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Brassin
+        /// </summary>
+        private string _brassin;
+        public string Brassin
+        {
+            get
+            {
+                return _brassin;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("Le houblonnage doit avoir des consignes.");
+                else
+                {
+                    _brassin = value;
+                    NotifyPropertyChanged("Brassin");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Fermentation
+        /// </summary>
+        private string _fermentation;
+        public string Fermentation
+        {
+            get
+            {
+                return _fermentation;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("La fermentation doit avoir des consignes.");
+                else
+                {
+                    _fermentation = value;
+                    NotifyPropertyChanged("Fermentation");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Embouteillage
+        /// </summary>
+        private string _embouteillage;
+        public string Embouteillage
+        {
+            get
+            {
+                return _embouteillage;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("L'embouteillage doit avoir des consignes.");
+                else
+                {
+                    _embouteillage = value;
+                    NotifyPropertyChanged("Embouteillage");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Conservation
+        /// </summary>
+        private string _conservation;
+        public string Conservation
+        {
+            get
+            {
+                return _conservation;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("La conservation doit avoir des consignes.");
+                else
+                {
+                    _conservation = value;
+                    NotifyPropertyChanged("Conservation");
+                }
+            }
+        }
+
+
+
+
+        #endregion
+
+        #region Constructeur
+        /// <summary>
+        /// Constructeur d'une Bière
+        /// </summary>
+        /// <param name="nom">Nom de la bière</param>
+        /// <param name="categorie">Catégorie de la bière</param>
+        /// <param name="type">Type de la bière</param>
+        /// <param name="description">Descritpion de la bière</param>
+        /// <param name="alcool">Taux d'alcool de la bière</param>
+        /// <param name="amertume">Amertume de la bière</param>
+        /// <param name="prix">Prix de la bière</param>
+        /// <param name="empatage">Description de l'empatage</param>
+        /// <param name="brassin">Descritpion du brassin</param>
+        /// <param name="fermentation">Description de la frmentation de la bière</param>
+        /// <param name="embouteillage">Description de l'embouteillage de la bière</param>
+        /// <param name="conservation">Description de la conservation de la bière</param>
+        /// <param name="nbbouteille">Nombre de bouteilles en stock </param>
+        public BiereModel(string nom, string categorie, string type, string description, float alcool, int amertume, float prix, string empatage, string brassin, string fermentation, string embouteillage, string conservation, string image, int nbbouteille)
+        {
+            try
+            {
+                Nom = nom;
+                Categorie = categorie;
+                Type = type;
+                Description = description;
+                Alcool = alcool;
+                Amertume = amertume;
+                Prix = prix;
+                Empatage = empatage;
+                Brassin = brassin;
+                Fermentation = fermentation;
+                Embouteillage = embouteillage;
+                Conservation = conservation;
+                ImageUrl = ImageUrl;
+                NbBouteille = nbbouteille;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur dans au moins un des arguments lors de la création d'une bière : {0}", e);
+            }
+        }
+
+        public BiereModel()
+        {
+        }
+        #endregion
+
+        #region Affichages
+        /// <summary>
+        /// Affichage d'une bière
+        /// </summary>
+        /// <returns>Affichage d'une bière</returns>
+        public override string ToString()
+        {
+            return string.Format("{0}", Nom);
+        }
+
+        /// <summary>
+        /// Affichage formatté du prix
+        /// </summary>
+        /// <returns>Affichage formatté du prix</returns>
+        public string AfficherPrix()
+        {
+            return string.Format("{0} euros", Prix);
+        }
+
+        /// <summary>
+        /// Affichage formatté de l'alcool
+        /// </summary>
+        /// <returns>Affichage formatté de l'alcool</returns>
+        public string AfficherAlcool()
+        {
+            return string.Format("{0}%", Alcool);
+        }
+
+        /// <summary>
+        /// Affichage formatté de l'amertume
+        /// </summary>
+        /// <returns>Affichage formatté de l'amertume</returns>
+        public string AfficherAmertume()
+        {
+            return string.Format("{0} sur 10", Amertume);
+        }
+
+        /// <summary>
+        /// Affichage formatté de la quantité disponible
+        /// </summary>
+        /// <returns>Affichage formatté de la quantité disponible</returns>
+        public string AfficherNbBouteille()
+        {
+            if(NbBouteille <= 0)
+                return string.Format("{0} bouteille", NbBouteille);
+            else
+                return string.Format("{0} bouteilles", NbBouteille);
+        }
+        #endregion
+
+    }
+}
---
+++ LaLaverieProject/Model/ClientModel.cs
@@ -0,0 +1,304 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaLaverie.Model
+{
+    public class ClientModel : NotifyPropertyChangedBase
+    {
+
+        #region Attributs et propriétés
+        /// <summary>
+        /// Nom du client
+        /// </summary>
+        private string _nom;
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("Le nom du client est invalide.");
+                else
+                {
+                    _nom = value;
+                    NotifyPropertyChanged("Nom");
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Prenom du client
+        /// </summary>
+        private string _prenom;
+        public string Prenom
+        {
+            get
+            {
+                return _prenom;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("Le prenom du client est invalide.");
+                else
+                {
+                    _prenom = value;
+                    NotifyPropertyChanged("Prenom");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Age du client
+        /// </summary>
+        private int _age;
+        public int  Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("L'age du client est invalide.");
+                else
+                {
+                    _age = value;
+                    NotifyPropertyChanged("Age");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Numéro de rue du client
+        /// </summary>
+        private int _numeroRue;
+        public int NumeroRue
+        {
+            get
+            {
+                return _numeroRue;
+            }
+            set
+            {
+                _numeroRue = value;
+                NotifyPropertyChanged("NumeroRue");
+            }
+        }
+
+        /// <summary>
+        /// Rue de l'adresse du client
+        /// </summary>
+        private string _rue;
+        public string Rue
+        {
+            get
+            {
+                return _rue;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("La rue de l'adresse du client est invalide.");
+                else
+                {
+                    _rue = value;
+                    NotifyPropertyChanged("Rue");
+                }
+                    
+            }
+        }
+
+        /// <summary>
+        /// Ville du client
+        /// </summary>
+        private string _ville;
+        public string Ville
+        {
+            get
+            {
+                return _ville;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("La ville du client est invalide.");
+                else
+                {
+                    _ville = value;
+                    NotifyPropertyChanged("Ville");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Code postal du client
+        /// </summary>
+        private int _codePostal;
+        public int CodePostal
+        {
+            get
+            {
+                return _codePostal;
+            }
+            set
+            {
+                if (!(value > 0 && value <= 99999))
+                    throw new Exception("Le code postal n'est pas correcte.");
+                else
+                {
+                    _codePostal = value;
+                    NotifyPropertyChanged("CodePostal");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adresse mail du client
+        /// </summary>
+        private string _mail;
+        public string Mail
+        {
+            get
+            {
+                return _mail;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    _mail = "Ce client ne possède pas d'adresse mail";
+                else
+                {
+                    _mail = value;
+                    NotifyPropertyChanged("Mail");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mot de passe du client
+        /// </summary>
+        private string _motDePasse;
+        public string MotDePasse
+        {
+            get
+            {
+                return _motDePasse;
+            }
+            set
+            {
+                if (value.Equals(null))
+                    throw new Exception("Le mot de passe ne peut être nul.");
+                else
+                {
+                    _motDePasse = value;
+                    NotifyPropertyChanged("MotDePasse");
+                }
+            }
+        }
+        #endregion
+
+        #region Constructeur
+        /// <summary>
+        /// Constructeur du client
+        /// </summary>
+        /// <param name="nom">Nom du client</param>
+        /// <param name="prenom">Prenom du client</param>
+        /// <param name="num">Numéro de rue du client</param>
+        /// <param name="rue">Rue de l'adresse du client</param>
+        /// <param name="ville">Ville du client</param>
+        /// <param name="codePostal">Code postal du client</param>
+        /// <param name="mail">Adresse mail du client</param>
+        /// <param name="mdp">Mot de passe du client</param>
+        /// <param name="age">Age du client</param>
+        public ClientModel(string nom, string prenom, int num, string rue, string ville, int codePostal, string mail, string mdp, int age)
+        {
+            try
+            {
+                Nom = nom;
+                Prenom = prenom;
+                NumeroRue = num;
+                Rue = rue;
+                Ville = ville;
+                CodePostal = codePostal;
+                Mail = mail;
+                MotDePasse = mdp;
+                Age = age;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur dans au moins un des arguments lors de la création d'un client : {0}", e);
+            }
+
+        }
+
+        public ClientModel(){}
+        #endregion
+
+        #region Methodes
+        /// <summary>
+        /// Détermine si le client en création existe déjà
+        /// </summary>
+        /// <param name="client">Client existant potentiellement déjà</param>
+        /// <returns>True si la personne existe sinon false</returns>
+        public bool Equals(ClientModel client)
+        {
+            if (this.CompareTo(client) == 0)
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Compare deux clients existants
+        /// </summary>
+        /// <param name="client">Un autre client</param>
+        /// <returns>1, 0 ou -1</returns>
+        public int CompareTo(ClientModel client)
+        {
+
+            if (client.Nom == Nom)
+            {
+                if (client.Prenom == Prenom)
+                {
+                    if (client.Rue == Rue)
+                    {
+                        if (client.NumeroRue == NumeroRue)
+                            return 0;
+                    }
+                }
+            }
+            else if (string.Compare(client.Nom, Nom) > 0)
+            {
+                if (string.Compare(client.Prenom, Prenom) > 0)
+                {
+                    if (string.Compare(client.Rue, Rue) > 0)
+                    {
+                        if (client.NumeroRue >= NumeroRue)
+                            return 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Affichage d'un client
+        /// </summary>
+        /// <returns>Les coordonnées du client au complet en format lettre</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Nom, Prenom);
+        }
+        #endregion
+    }
+}
